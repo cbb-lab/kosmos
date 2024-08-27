42a5736721c255ab7a20fd6bac541eda421be344
Chanbok Wi <cksqhr4961@gmail.com>
2024-08-28 00:11:03 +0900

    refactor: Update build.gradle.kts for multi-module project

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,15 +2,17 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.9.25"
-    kotlin("plugin.spring") version "1.9.25" apply false
-    id("org.springframework.boot") version "3.3.3" apply false
-    id("io.spring.dependency-management") version "1.1.6" apply false
+    kotlin("jvm")
+    kotlin("kapt")
+    kotlin("plugin.spring") apply false
+    kotlin("plugin.jpa") apply false
+    id("org.springframework.boot") apply false
+    id("io.spring.dependency-management")
 }
 
 allprojects {
-    group = "io"
-    version = "0.0.1-SNAPSHOT"
+    group = "${property("projectGroup")}"
+    version = "${property("applicationVersion")}"
 
     repositories {
         mavenCentral()
@@ -19,11 +21,15 @@ allprojects {
 
 subprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "org.jetbrains.kotlin.kapt")
     apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+    apply(plugin = "org.jetbrains.kotlin.plugin.jpa")
     apply(plugin = "org.springframework.boot")
     apply(plugin = "io.spring.dependency-management")
 
     dependencies {
+        annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+        kapt("org.springframework.boot:spring-boot-configuration-processor")
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
         implementation("org.jetbrains.kotlin:kotlin-reflect")
         testImplementation("org.springframework.boot:spring-boot-starter-test")
@@ -33,7 +39,7 @@ subprojects {
 
     tasks.withType<KotlinCompile> {
         compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_21)
+            jvmTarget.set(JvmTarget.valueOf("JVM_${project.property("javaVersion")}"))
             freeCompilerArgs.addAll("-Xjsr305=strict")
         }
     }
@@ -51,4 +57,4 @@ subprojects {
     }
 }
 
-java.toolchain.languageVersion = JavaLanguageVersion.of(21)
+java.toolchain.languageVersion = JavaLanguageVersion.of("${project.property("javaVersion")}")
